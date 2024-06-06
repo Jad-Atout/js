f48b8e66a87ed65cafaaf68054438aeebf4c8788
Jad-Atout <JadAtout.2003@outlook.sa>
2024-06-06 21:20:42 +0300

    kj

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-document.querySelector(".userProfile").textContent="Jad Atout"
-document.querySelector(".userInfo p").classList.add("ja")
+
+
+var small_button = document.querySelector("#small");
+var big_button = document.querySelector("#big");
+var h = document.querySelector("h2");
+var p = document.querySelector("p");
+
+function increase(){
+    h.classList.add("hsize");
+    p.classList.add("psize");
+}
+function decrease(){
+    h.classList.remove("hsize");
+    p.classList.remove("psize");
+}
+small_button.onclick = decrease;
+big_button.onclick = increase;
